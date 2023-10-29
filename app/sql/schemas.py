e5b00d2692b5d51686222e394db135d7e2752a44
İlhan Yavuz İskurt <ilhaniskurt@Ilhans-MacBook-Air.local>
2023-10-29 23:08:31 +0300

    remove items

--- app/sql/schemas.py
+++ app/sql/schemas.py
@@ -1,24 +1,8 @@
 from pydantic import BaseModel
 
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        from_attributes = True
-
-
 class UserBase(BaseModel):
+    username: str
     email: str
 
 
@@ -28,8 +12,6 @@ class UserCreate(UserBase):
 
 class User(UserBase):
     id: int
-    is_active: bool
-    items: list[Item] = []
 
     class Config:
         from_attributes = True

6028368132d788ddcbe8d96564d9173a7f3be02e
İlhan Yavuz İskurt <ilhaniskurt@192.168.68.61>
2024-01-22 19:09:00 +0300

    recommendations updated

--- app/recommend.py
+++ app/recommend.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from functools import lru_cache
+from typing import Annotated
 
 import pandas as pd
 from fastapi import APIRouter, Depends, FastAPI, HTTPException
@@ -59,6 +60,25 @@ def get_features_from_model(model: models.Song | models.Album):
     return features
 
 
+def get_features_from_models(models: list[models.Song] | list[models.Album]):
+    features = [
+        [
+            model.danceability,
+            model.speechiness,
+            model.acousticness,
+            model.instrumentalness,
+            model.liveness,
+            model.valence,
+            model.tempo,
+            model.loudness,
+            model.mode,
+            model.key,
+        ]
+        for model in models
+    ]
+    return features
+
+
 @lru_cache
 def get_features_from_df():
     features = default_songs[
@@ -102,6 +122,129 @@ def recommend_song_from_song(
     recommendations = []
 
     if len(similarities) < 1 + recommend:
+        raise HTTPException(
+            status_code=404,
+            detail="Could not find the required amount of recommendation(s)",
+        )
+
+    for i in range(recommend):
+        similarities[similarities.argmax()] = -1
+        # max_similarity = similarities.max()
+        recommended_song_id = default_songs.loc[similarities.argmax(), "id"]
+
+        recommendations.append(crud.get_song_by_id(db, recommended_song_id))
+
+    return recommendations
+
+
+@router.get("/album/{id}", response_model=list[schemas.Song])
+def recommend_song_from_album(
+    id: str,
+    recommend: int = 10,
+    db: Session = Depends(dependencies.get_db),
+):
+    album = crud.get_album_by_id(db, id)
+    if not album:
+        raise HTTPException(status_code=404, detail="Album not found")
+
+    if album.owner_id != 0:
+        raise HTTPException(
+            status_code=400,
+            detail="User registered songs are incompatible for recommendation",
+        )
+
+    tracks = crud.get_songs_by_album_id(db, id)
+    if not tracks:
+        raise HTTPException(status_code=404, detail="Album is empty")
+
+    target_song_features = get_features_from_models(tracks)
+    all_song_features = get_features_from_df()
+
+    similarities = cosine_similarity(target_song_features, all_song_features)[0]
+
+    recommendations = []
+
+    if len(similarities) < len(tracks) + recommend:
+        raise HTTPException(
+            status_code=404,
+            detail="Could not find the required amount of recommendation(s)",
+        )
+
+    for i in range(recommend):
+        similarities[similarities.argmax()] = -1
+        # max_similarity = similarities.max()
+        recommended_song_id = default_songs.loc[similarities.argmax(), "id"]
+
+        recommendations.append(crud.get_song_by_id(db, recommended_song_id))
+
+    return recommendations
+
+
+@router.get("/playlist/{id}", response_model=list[schemas.Song])
+def recommend_song_from_playlist(
+    id: int,
+    recommend: int = 10,
+    db: Session = Depends(dependencies.get_db),
+):
+    playlist = crud.get_playlist_by_id(db, id)
+    if not playlist:
+        raise HTTPException(status_code=404, detail="Playlist not found")
+
+    tracks = [track for track in playlist.songs if track.owner_id == 0]
+
+    if not tracks:
+        raise HTTPException(
+            status_code=404,
+            detail="There is no non-user register song in this playlist",
+        )
+
+    target_song_features = get_features_from_models(tracks)
+    all_song_features = get_features_from_df()
+
+    similarities = cosine_similarity(target_song_features, all_song_features)[0]
+
+    recommendations = []
+
+    if len(similarities) < len(tracks) + recommend:
+        raise HTTPException(
+            status_code=404,
+            detail="Could not find the required amount of recommendation(s)",
+        )
+
+    for i in range(recommend):
+        similarities[similarities.argmax()] = -1
+        # max_similarity = similarities.max()
+        recommended_song_id = default_songs.loc[similarities.argmax(), "id"]
+
+        recommendations.append(crud.get_song_by_id(db, recommended_song_id))
+
+    return recommendations
+
+
+@router.get("/starred", response_model=list[schemas.Song])
+def recommend_song_from_starred(
+    current_user: Annotated[models.User, Depends(dependencies.get_current_user)],
+    recommend: int = 10,
+    db: Session = Depends(dependencies.get_db),
+):
+    starred = crud.get_starred(db, current_user.id)
+
+    tracks = [track for track in starred.songs if track.owner_id == 0]
+
+    if not tracks:
+        raise HTTPException(
+            status_code=404,
+            detail="There is no non-user register song starred",
+        )
+
+    target_song_features = get_features_from_models(tracks)
+    all_song_features = get_features_from_df()
+
+    similarities = cosine_similarity(target_song_features, all_song_features)[0]
+
+    recommendations = []
+
+    if len(similarities) < len(tracks) + recommend:
         raise HTTPException(
             status_code=404,
             detail="Could not find the required amount of recommendation(s)",
